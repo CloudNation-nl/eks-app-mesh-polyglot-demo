2e32bcff7bed4f3995db8e972d2178e44bb69db3
luuk rutten <luuk.rutten@cloudnation.nl>
2022-11-01 17:24:20 +0100

    add AWS_REGION to env vars

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ source ~/.bashrc
 aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE
 rm -vf ${HOME}/.aws/credentials
 export AWS_DEFAULT_REGION=eu-west-1
+export AWS_REGION=eu-west-1
 export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name eksworkshop-eksctl-2
 kubectl get nodes
