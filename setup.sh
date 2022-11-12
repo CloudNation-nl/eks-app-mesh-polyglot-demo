e2e23ea18c3ba27236488683f2c6e0da93055bea
luuk rutten <luuk.rutten@cloudnation.nl>
2022-11-12 17:02:19 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -5,15 +5,14 @@ sudo curl --silent --location -o /usr/local/bin/kubectl \
    https://amazon-eks.s3.us-west-2.amazonaws.com/1.20.4/2021-04-12/bin/linux/amd64/kubectl
 
 sudo chmod +x /usr/local/bin/kubectl
-unzip awscliv2.zip
-sudo ./aws/install
+
 sudo yum -y install jq gettext bash-completion moreutils
 echo 'yq() {
   docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
 }' | tee -a ~/.bashrc && source ~/.bashrc
 
 npm install -g c9
-curl -sS https://webinstall.dev/k9s | bash
+# curl -sS https://webinstall.dev/k9s | bash
 for command in kubectl jq envsubst aws
   do
     which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
@@ -28,10 +27,11 @@ sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/l
 echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name -L karpenter.sh/capacity-type'" | tee -a ~/.bashrc
 source ~/.bashrc
 aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE
-rm -vf ${HOME}/.aws/credentials
+# rm -vf ${HOME}/.aws/credentials
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_REGION=eu-west-1
 export ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+rm ~/.kube/config
 aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name eksworkshop-eksctl
 kubectl get nodes
 
@@ -40,4 +40,4 @@ helm version --short
 helm list
 
 
-eksctl set
+# eksctl set
